476f31825e3e7f8239856b2f7e0f11574cd2bf74
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-08-27 17:12:59 -0300

    Initial commit

---
+++ RequestAuthorizations/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RequestAuthorizations
+//
+//  Created by Jonattan Sousa on 27/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+}
+
