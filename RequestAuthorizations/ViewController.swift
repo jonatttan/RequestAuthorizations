6f64a079aabb03e9e0d65be8a407d90174031492
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-09-02 10:50:56 -0300

    Implements request access for Photo library and Camera

--- RequestAuthorizations/ViewController.swift
+++ RequestAuthorizations/ViewController.swift
@@ -6,13 +6,52 @@
 //
 
 import UIKit
+import AVKit
+import Photos
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.view.backgroundColor = .red
+//        verifyAvailability()
+        requestCameraAccess()
+        requestPhotoLibraryAccess()
+    }
+    
+    // MARK: - Just note, this verify availability of resources
+    func verifyAvailability() {
+        let cameraAccess = UIImagePickerController.isSourceTypeAvailable(.camera)
+        let photoLibraryAccess = UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        print("has camera: ", cameraAccess)
+        print("has Photo Library: ", photoLibraryAccess)
+    }
+    
+    func requestCameraAccess() {
+        AVCaptureDevice.requestAccess(for: .video) { hasAccess in
+            print("Has camera access?: ", hasAccess)
+        }
+    }
+    
+    func requestPhotoLibraryAccess() {
+        var authStatus = ""
+        let photoLibrary = PHPhotoLibrary.authorizationStatus()
+        if photoLibrary == .notDetermined {
+            PHPhotoLibrary.requestAuthorization { authorization in
+                switch authorization {
+                case .authorized:
+                    authStatus = "Authorized"
+                case .restricted:
+                    authStatus = "Restricted"
+                case .limited:
+                    authStatus = "Limited"
+                default:
+                    authStatus = "Denied"
+                }
+                print("Photo library access is: ", authStatus)
+            }
+        }
     }
 }
 
+//class ImageChoice: NSObject
