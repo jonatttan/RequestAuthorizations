c745818c3d7b7b614b2b7fc2dc995cc41a9adaf0
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-09-03 11:43:31 -0300

    Implements notifications request use

--- RequestAuthorizations/ViewController.swift
+++ RequestAuthorizations/ViewController.swift
@@ -8,11 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let center = UNUserNotificationCenter.current()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.view.backgroundColor = .red
+        manageNotificationAccess()
     }
+    
+    func manageNotificationAccess() {
+        Task {
+            do {
+                try await notificationsRequestUse()
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+            await haveNotificationsAccess { hasAccess in
+                print("Has notification access: ", hasAccess)
+            }
+        }
+    }
+    
+    func haveNotificationsAccess(completion: @escaping(Bool) -> Void) async {
+        let settings = await center.notificationSettings()
+        
+        guard settings.authorizationStatus == .authorized else {
+            return completion(false)
+        }
+        completion(true)
+    }
+    
+    func notificationsRequestUse() async throws {
+        try await center.requestAuthorization(options: [.alert, .sound, .badge])
+    }
+    
+    // Calendar
+    // Contacts
+    // FaceId
 }
+
 
